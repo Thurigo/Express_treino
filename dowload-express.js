0f005dc83ed1a3ba6222f9e5ee05ea560f636630
Thurigo <arthur.spier@gmail.com>
2023-11-09 00:55:10 -0300

    Rota de download - Pronta e funcionando Uhull

--- dowload-express.js
+++ dowload-express.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 30000; 
+const port = 30000;
 
 
-app.get('/download', (req,res) => {
-    console.log('deu certo')
-    res.download('./public', 'Arthur.png')
+app.get('/download', (req, res) => {
+    console.log('Certo ')
+    res.download('./public/Arthur.png', 'Arthur.png', (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erro ao fazer o download.');
+        } else {
+            console.log('Download bem-sucedido.');
+        }
+    });
+    console.log('Certo2 ')
 })
 
 
